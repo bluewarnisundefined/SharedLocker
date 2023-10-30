ce3be041e9bd36c0fcac81fd0a5aae7e1893db14
20213846 <n5lp97@gmail.com>
2023-10-30 17:36:19 +0900

    폰트 추가

--- index.js
+++ index.js
@@ -2,8 +2,26 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { PaperProvider, configureFonts } from 'react-native-paper';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  const baseFont = {
+    fontFamily: 'PretendardVariable',
+  }
+
+  const baseVariants = configureFonts({ config: baseFont });
+  
+  return (
+    <PaperProvider theme={{
+      fonts: baseVariants
+    }}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+
+AppRegistry.registerComponent(appName, () => Main);
