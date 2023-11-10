a9003172309b6ca138e38e94bc5a0ed3563d5497
20213846 <n5lp97@gmail.com>
2023-11-10 21:40:44 +0900

    chore: 디렉토리 추가에 따른 alias 설정 추가, .env 관련 캐싱 문제 해결

--- babel.config.js
+++ babel.config.js
@@ -8,10 +8,22 @@ module.exports = {
         alias: {
           "@components": "./src/components",
           "@screens": "./src/screens",
+          "@network": "./src/network",
           "@assets": "./src/assets",
           "@utils": "./src/utils",
           '@': './src',
         },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
       },
     ],
   ],
