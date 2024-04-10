2f3f3255e722d0c81d7a57eabe9bf59a5413351a
bluewarn <n5lp97@gmail.com>
2024-04-10 15:47:07 +0900

    feat: 인증 헤더를 위한 인터셉터 정의

--- src/network/client.ts
+++ src/network/client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {API_BASE_URL} from '@env';
+import { getSecureToken } from '@/utils/keychain';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -7,3 +8,26 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+   async config => {
+    if(config.url === '/auth/login' || config.url === '/auth/register') {
+      return config;
+    }
+    // Get token from storage
+    const token = await getSecureToken('accessToken');
+
+    if (!token) {
+      return Promise.reject('access token 값이 유효하지 않습니다.');
+    }
+    
+    if(token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
