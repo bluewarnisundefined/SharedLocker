e72616642e1311f6abf76f4ac3b018d147f6a89d
bluewarn <n5lp97@gmail.com>
2024-04-27 17:01:11 +0900

    feat: user api 추가

--- src/network/user/api.ts
+++ src/network/user/api.ts
@@ -1,6 +1,9 @@
 import {axiosInstance} from '../client';
 
 const userAPI = () => ({
+  user: () => {
+    return axiosInstance.get('/api/user');
+  },
   locker: () => {
     return axiosInstance.get('/api/user/locker');
   },
