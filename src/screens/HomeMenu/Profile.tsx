58667000b321ab836393599df1a1691990f729d4
bluewarn <n5lp97@gmail.com>
2024-04-27 17:26:29 +0900

    feat: 회원 등록 정보 스크린 추가

--- src/screens/HomeMenu/Profile.tsx
+++ src/screens/HomeMenu/Profile.tsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
 import { HomeMenuStackScreenProps } from "@/navigation/types";
+import userAPI from "@/network/user/api";
+import { useQuery } from "@tanstack/react-query";
 import { Text } from "react-native-paper";
 
 export function Profile(props: HomeMenuStackScreenProps<'Profile'>): JSX.Element {
+  const {
+    data: userData,
+  } = useQuery(['user'], () => userAPI().user());
+  
   return (
     <Layout>
-      <Text>Profile</Text>
+      <Text>회원 등록 정보</Text>
+      <Text>아이디: {userData?.data?.message.userId}</Text>
+      <Text>이름: {userData?.data?.message.nickname}</Text>
+      <Text>이메일: {userData?.data?.message.email}</Text>
+      <Text>등록일: {userData?.data?.message.createdAt}</Text>
     </Layout>
   );
 }
