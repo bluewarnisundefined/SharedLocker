44dc93d26ad9d70372654e2f0975f184218b99dc
bluewarn <n5lp97@gmail.com>
2024-05-24 12:57:29 +0900

    로그 삭제

--- src/utils/keychain.ts
+++ src/utils/keychain.ts
@@ -3,14 +3,10 @@ import * as Keychain from 'react-native-keychain';
 type TokenType = 'accessToken' | 'refreshToken';
 
 export function setSecureToken(value: string, key: TokenType) {
-  console.log(`[setSecureToken] key: ${key}`);
   return Keychain.setGenericPassword(key, value, {service: key});
 }
 
 export function setSecureTokens(accessToken: string, refreshToken: string) {
-  console.log(
-    `[setSecureTokens] accessToken: ${accessToken}, refreshToken: ${refreshToken}`,
-  );
   return Promise.all([
     Keychain.setGenericPassword('accessToken', accessToken, {
       service: 'accessToken',
@@ -22,10 +18,8 @@ export function setSecureTokens(accessToken: string, refreshToken: string) {
 }
 
 export async function getSecureToken(key: TokenType) {
-  console.log(`[getSecureToken] key: ${key}`);
   const result = await Keychain.getGenericPassword({service: key});
 
-  console.log('[getSecureToken] token: ', result);
   if (result) {
     return result.password;
   }
@@ -33,12 +27,10 @@ export async function getSecureToken(key: TokenType) {
 }
 
 export function removeSecureToken(key: TokenType) {
-  console.log(`[removeSecureToken] key: ${key}`);
   return Keychain.resetGenericPassword({service: key});
 }
 
 export function removeAllSecureToken() {
-  console.log('[removeAllSecureToken]');
   return Promise.all([
     Keychain.resetGenericPassword({service: 'accessToken'}),
     Keychain.resetGenericPassword({service: 'refreshToken'}),
